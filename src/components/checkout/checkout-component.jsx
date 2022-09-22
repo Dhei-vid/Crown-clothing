d0504818590dc7804893581b0f2053f1cb096c34
Dhei-vid <david.etuk@aun.edu.ng>
2022-09-22 17:14:52 +0100

    building the checkout page

--- src/components/checkout/checkout-component.jsx
+++ src/components/checkout/checkout-component.jsx
@@ -1,5 +1,13 @@
+import { useContext } from "react";
 import "./checkout-styles.scss";
 
-const CheckOut = () => {};
+const CheckOut = () => {
+  return (
+    <div>
+      <h1>Item 1 </h1>
+      <span> $50</span>
+    </div>
+  );
+};
 
 export default CheckOut;
