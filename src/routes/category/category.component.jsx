56918b13b14c8260b731d5272b66bd5ed371205e
Dhei-vid <david.etuk@aun.edu.ng>
2022-09-27 19:15:46 +0100

    created the category page for displaying the categories dynamically

---
+++ src/routes/category/category.component.jsx
@@ -0,0 +1,29 @@
+import { useContext, useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { CategoriesContext } from "../../context/categories.context";
+import ProductCard from "../../components/product-card/product-card.component";
+
+import "./category-styles.scss";
+
+const Category = () => {
+  const { category } = useParams();
+  const { categoriesMap } = useContext(CategoriesContext);
+  const [products, setProducts] = useState(categoriesMap[category]);
+
+  useEffect(() => {
+    setProducts(categoriesMap[category]);
+  }, [category, categoriesMap]);
+
+  return (
+    <div className="category-container">
+      {
+        // this is a safe guard built so the code does not run unless products evaluates to true
+        // Good practice for when you have data that runs asynchronously
+        products &&
+          products.map((prod) => <ProductCard key={prod.id} product={prod} />)
+      }
+    </div>
+  );
+};
+
+export default Category;
