006bff5bc942887ac2f61fc5129ea11b122267c3
Dhei-vid <david.etuk@aun.edu.ng>
2022-09-28 14:37:52 +0100

    Linked the header to category

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -15,14 +15,21 @@ const Category = () => {
   }, [category, categoriesMap]);
 
   return (
-    <div className="category-container">
-      {
-        // this is a safe guard built so the code does not run unless products evaluates to true
-        // Good practice for when you have data that runs asynchronously
-        products &&
-          products.map((prod) => <ProductCard key={prod.id} products={prod} />)
-      }
-    </div>
+    <>
+      <div>
+        <h2>{category.toUpperCase()}</h2>
+      </div>
+      <div className="category-container">
+        {
+          // this is a safe guard built so the code does not run unless products evaluates to true
+          // Good practice for when you have data that runs asynchronously
+          products &&
+            products.map((prod) => (
+              <ProductCard key={prod.id} products={prod} />
+            ))
+        }
+      </div>
+    </>
   );
 };
 
--- src/routes/navigation/navigation-component.jsx
+++ src/routes/navigation/navigation-component.jsx
@@ -3,7 +3,6 @@ import { Outlet, Link } from "react-router-dom";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { UserContext } from "../../context/user.context";
 import { CartContext } from "../../context/cartContext";
-
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import CartIcon from "../../components/cart-icon/cart-icon-component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown-component";
