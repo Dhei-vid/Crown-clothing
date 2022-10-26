9e01be8b9af99493972920302eda3ba170574464
Dhei-vid <david.etuk@aun.edu.ng>
2022-10-26 04:50:10 +0100

    understanding logger

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -9,13 +9,13 @@ import { CategoryContainer } from "./category-styles";
 
 const Category = () => {
   const { category } = useParams();
-  console.log("render category component");
+  console.log("render/re-rendering category component");
   const categoriesMap = useSelector(selectCategoryMap);
 
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
-    console.log("effect fired");
+    console.log("effect fired calling setProducts");
     setProducts(categoriesMap[category]);
   }, [category, categoriesMap]);
 
--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,6 @@
 import { compose, createStore, applyMiddleware } from "redux";
 
-import { logger } from "redux-logger";
+// import { logger } from "redux-logger";
 
 import { rootReducers } from "./rootReducer";
 
@@ -10,7 +10,16 @@ import { rootReducers } from "./rootReducer";
  * createStore is deprecated in redux 4.2.0 so I had to downgrade to redux 4.1.2
  * logger lets us see what the state looks like before the action is dispatched, the action and the state after the action
  * Middleware are action libraries that run before the action gets to the reducer
+ * Currying a function (a function that returns another function)
  */
+
+const loggerMiddleWare = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
+  console.log("type", action.type);
+  console.log("payload", action.payload);
+};
 
 const middleWare = [process.env.NODE_ENV === "development" && logger].filter(
   Boolean
