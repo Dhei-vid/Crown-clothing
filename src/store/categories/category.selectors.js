a0ccb685fd46e585b4779f3b99ff5f1057cf39b0
Dhei-vid <david.etuk@aun.edu.ng>
2022-10-27 16:00:58 +0100

    still on the memoized selector

--- src/store/categories/category.selectors.js
+++ src/store/categories/category.selectors.js
@@ -1,10 +1,9 @@
 import { createSelector } from "reselect";
 
 // To extend into selectors, we have to create input selectors and output selectors.
-
 const selectCategoryReducer = (state) => state.category;
 
-// This is memoized selector
+// createSelector create a memoized selector (takes 2 arg)
 // the only time this will be rerun is if the input value (category object - line 5) is different
 export const selectCategories = createSelector(
   [selectCategoryReducer],
