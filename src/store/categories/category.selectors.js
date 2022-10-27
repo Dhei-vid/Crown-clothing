6bb426bc64697d390ac54f2a223632517037044e
Dhei-vid <david.etuk@aun.edu.ng>
2022-10-27 16:49:19 +0100

    transitioning from cart context to cart reducers with redux

--- src/store/categories/category.selectors.js
+++ src/store/categories/category.selectors.js
@@ -3,7 +3,7 @@ import { createSelector } from "reselect";
 // To extend into selectors, we have to create input selectors and output selectors.
 const selectCategoryReducer = (state) => state.category;
 
-// createSelector create a memoized selector (takes 2 arg)
+// createSelector create a memoized selector (takes 2 arg - input selector and output selector)
 // the only time this will be rerun is if the input value (category object - line 5) is different
 export const selectCategories = createSelector(
   [selectCategoryReducer],
@@ -12,12 +12,10 @@ export const selectCategories = createSelector(
 
 export const selectCategoryMap = createSelector(
   [selectCategories],
-  (categories) => {
-    console.log("memoized", categories);
-    return categories.reduce((acc, category) => {
+  (categories) =>
+    categories.reduce((acc, category) => {
       const { title, items } = category;
       acc[title.toLowerCase()] = items;
       return acc;
-    }, {});
-  }
+    }, {})
 );
