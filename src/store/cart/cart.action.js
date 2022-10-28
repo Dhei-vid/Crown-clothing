9f7017913fa19a011611e009fa285246aca9859b
Dhei-vid <david.etuk@aun.edu.ng>
2022-10-28 18:00:50 +0100

    developing the cart selector

--- src/store/cart/cart.action.js
+++ src/store/cart/cart.action.js
@@ -1,6 +1,21 @@
 import CART_ACTION_TYPES from "./cart.types";
 import { createAction } from "../../utils/reducers/reducers.utils";
 
+// SET_CURRENT_TOTAL: "cart/SET_CURRENT_TOTAL",
+// SET_CURRENT_COUNT: "cart/SET_CURRENT_COUNT",
+// SET_CART_STATUS: "cart/SET_CART_STATUS",
+// SET_CART_ITEM: "cart/SET_CART_ITEM",
+
 export const setCart = (cart) => {
-  return createAction();
+  const cartItem = createAction(CART_ACTION_TYPES.SET_CART_ITEM, cart);
+  const total = createAction(CART_ACTION_TYPES.SET_CURRENT_TOTAL, cart);
+  const cartStatus = createAction(CART_ACTION_TYPES.SET_CART_STATUS, cart);
+  const count = createAction(CART_ACTION_TYPES.SET_CURRENT_COUNT, cart);
+
+  // console.log("cartItem", cartItem);
+  // console.log("total", total);
+  // console.log("cartStatus", cartStatus);
+  // console.log("count", count);
+
+  return { cartItem, total, cartStatus, count };
 };
--- src/store/cart/cart.selectors.js
+++ src/store/cart/cart.selectors.js
@@ -1,3 +1,33 @@
 import { createSelector } from "reselect";
 
 const selectCartReducer = (state) => state.category;
+
+export const selectCart = createSelector(
+  [selectCartReducer],
+  (cartslice) => cartslice.cart
+);
+
+// this is where I get to transform the data
+export const selectCartMap = createSelector([selectCart], (cart) =>
+  cart.reduce((acc, category) => {
+    const { title, items } = category;
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {})
+);
+
+const addCartItem = (cartItems, productToAdd) => {
+  // find if cartitems contain productToadd
+  const foundItems = cartItems.find((item) => item.id === productToAdd.id);
+  // if found increment quantity (what we want is to return a new array)
+  if (foundItems) {
+    return cartItems.map((items) =>
+      items.id === productToAdd.id
+        ? { ...items, quantity: items.quantity + 1 }
+        : items
+    );
+  }
+
+  // return new array with modified cartitems/ new cart items.
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
