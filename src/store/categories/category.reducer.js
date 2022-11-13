bbeb76cb58d07d6d63a3518b65433bab0861f876
Dhei-vid <david.etuk@aun.edu.ng>
2022-11-13 19:32:24 +0100

    changing the category types and reducers to model async redux thunk patterns

--- src/store/categories/category.reducer.js
+++ src/store/categories/category.reducer.js
@@ -2,16 +2,34 @@ import CATEGORY_ACTION_TYPES from "./category.types";
 
 export const INITIAL_STATE = {
   categories: [],
+  isLoading: false,
+  error: null,
 };
 
 export const categoryReducer = (state = INITIAL_STATE, action = {}) => {
   const { type, payload } = action;
 
+  console.log("Payload", payload);
+  console.log("type", type);
+
   switch (type) {
-    case CATEGORY_ACTION_TYPES.SET_CURRENT_CATEGORY:
+    case CATEGORY_ACTION_TYPES.FETCH_CURRENT_CATEGORY_START:
+      return {
+        ...state,
+        isLoading: true,
+        categories: payload,
+      };
+    case CATEGORY_ACTION_TYPES.FETCH_CURRENT_CATEGORY_START:
       return {
         ...state,
         categories: payload,
+        isLoading: false,
+      };
+    case CATEGORY_ACTION_TYPES.FETCH_CURRENT_CATEGORY_FAIL:
+      return {
+        ...state,
+        error: payload,
+        isLoading: false,
       };
     default:
       return state;
