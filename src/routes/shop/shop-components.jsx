528eb6818da2b1c46b893763606031c23f26b967
Dhei-vid <david.etuk@aun.edu.ng>
2022-11-13 19:55:27 +0100

    setting up the categories action redux thunk logic

--- src/routes/shop/shop-components.jsx
+++ src/routes/shop/shop-components.jsx
@@ -3,8 +3,7 @@ import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
-import { setCategories } from "../../store/categories/category.action";
+import { fetchCategoryAsync } from "../../store/categories/category.action";
 
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
@@ -13,14 +12,7 @@ const Shop = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const getCategoriesMap = async () => {
-      const categories = await getCategoriesAndDocuments("categories");
-
-      dispatch(setCategories(categories));
-    };
-
-    getCategoriesMap();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    dispatch(fetchCategoryAsync(categories));
   }, []);
 
   return (
--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,6 +1,7 @@
 import CATEGORY_ACTION_TYPES from "./category.types";
 
 import { createAction } from "../../utils/reducers/reducers.utils";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 
 // export const setCategories = (categories) => {
 //   return createAction(CATEGORY_ACTION_TYPES.SET_CURRENT_CATEGORY, categories);
@@ -22,4 +23,15 @@ export const fetchCategoryFailed = (error) => {
     CATEGORY_ACTION_TYPES.FETCH_CURRENT_CATEGORY_FAILED,
     error
   );
+};
+
+export const fetchCategoryAsync = () => async (dispatch) => {
+  dispatch(fetchCategoryStart());
+  try {
+    const categories = getCategoriesAndDocuments(categories);
+
+    dispatch(fetchCategorySuccess(categories));
+  } catch (error) {
+    dispatch(fetchCategoryFailed(error.message));
+  }
 };
