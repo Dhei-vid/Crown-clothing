85c7f22fb1fb3831c2bc9bc1579efa4c40e6e170
Dhei-vid <david.etuk@aun.edu.ng>
2022-11-26 20:49:34 +0100

    updating the user action to reflect the saga migration

--- src/store/user/user.action.js
+++ src/store/user/user.action.js
@@ -5,3 +5,26 @@ import { createAction } from "../../utils/reducers/reducers.utils";
 export const setCurrentUser = (user) => {
   return createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user);
 };
+
+/*
+ CHECK_USER_SESSION_START: "user/CHECK_USER_SESSION_START",
+  GOOGLE_SIGN_IN_START: "user/GOOGLE_SIGN_IN_START",
+  EMAIL_SIGN_IN_START: "user/EMAIL_SIGN_IN_START",
+  SIGN_IN_SUCCESS: "user/SIGN_IN_SUCCESS",
+  SIGN_IN_FAILURE: "user/SIGN_IN_FAILURE",
+*/
+
+export const checkUserSession = () =>
+  createAction(USER_ACTION_TYPES.CHECK_USER_SESSION_START);
+
+export const GoogleSignInStart = () =>
+  createAction(USER_ACTION_TYPES.GOOGLE_SIGN_IN_START);
+
+export const EmailSignInStart = (email, password) =>
+  createAction(USER_ACTION_TYPES.EMAIL_SIGN_IN_START, { email, password });
+
+export const userSignInSuccess = (user) =>
+  createAction(USER_ACTION_TYPES.SIGN_IN_SUCCESS, user);
+
+export const userSignInFailure = (error) =>
+  createAction(USER_ACTION_TYPES.SIGN_IN_FAILED, error);
