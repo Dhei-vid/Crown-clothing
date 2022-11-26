b5818e4f57bc4b657bfab468282e24fc76b12fe5
Dhei-vid <david.etuk@aun.edu.ng>
2022-11-26 20:54:24 +0100

    updating the user reducer

--- src/store/user/user.action.js
+++ src/store/user/user.action.js
@@ -6,14 +6,6 @@ export const setCurrentUser = (user) => {
   return createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user);
 };
 
-/*
- CHECK_USER_SESSION_START: "user/CHECK_USER_SESSION_START",
-  GOOGLE_SIGN_IN_START: "user/GOOGLE_SIGN_IN_START",
-  EMAIL_SIGN_IN_START: "user/EMAIL_SIGN_IN_START",
-  SIGN_IN_SUCCESS: "user/SIGN_IN_SUCCESS",
-  SIGN_IN_FAILURE: "user/SIGN_IN_FAILURE",
-*/
-
 export const checkUserSession = () =>
   createAction(USER_ACTION_TYPES.CHECK_USER_SESSION_START);
 
--- src/store/user/user.reducer.js
+++ src/store/user/user.reducer.js
@@ -2,6 +2,8 @@ import USER_ACTION_TYPES from "./user.types";
 
 export const INITIAL_STATE = {
   currentUser: null,
+  isLoading: false,
+  error: null,
 };
 
 export const userReducer = (state = INITIAL_STATE, action = {}) => {
@@ -9,12 +11,45 @@ export const userReducer = (state = INITIAL_STATE, action = {}) => {
   // the action takes two arguments, the type(could be string) and the payload (which is the new state value)
   const { type, payload } = action;
 
+  /*
+ CHECK_USER_SESSION_START: "user/CHECK_USER_SESSION_START",
+  GOOGLE_SIGN_IN_START: "user/GOOGLE_SIGN_IN_START",
+  EMAIL_SIGN_IN_START: "user/EMAIL_SIGN_IN_START",
+  SIGN_IN_SUCCESS: "user/SIGN_IN_SUCCESS",
+  SIGN_IN_FAILED: "user/SIGN_IN_FAILURE",
+*/
+
   // In redux we could get the action for all the reducers, and return the state.
   switch (type) {
     case USER_ACTION_TYPES.SET_CURRENT_USER:
       return {
         ...state,
         currentUser: payload,
+      };
+    case USER_ACTION_TYPES.CHECK_USER_SESSION_START:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    case USER_ACTION_TYPES.GOOGLE_SIGN_IN_START:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    case USER_ACTION_TYPES.EMAIL_SIGN_IN_START:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    case USER_ACTION_TYPES.SIGN_IN_SUCCESS:
+      return {
+        ...state,
+        currentUser: payload,
+      };
+    case USER_ACTION_TYPES.SIGN_IN_FAILED:
+      return {
+        ...state,
+        error: payload,
       };
     default:
       return state;
