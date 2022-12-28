c968b7d03dedf958b55568d19fd080287a71227e
David <djetuk@gmail.com>
2022-12-28 16:02:05 +0100

    integrating the stripe payment gateway - creating folder and files

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
-import React, { useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import React, { useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
 
-import Home from "./routes/home/home-components.jsx";
-import Navigation from "./routes/navigation/navigation-component.jsx";
-import Authentication from "./routes/Authentication/authentication-component.jsx";
-import Shop from "./routes/shop/shop-components.jsx";
-import CheckOut from "./routes/checkout/checkout-component.jsx";
+import Home from './routes/home/home-components.jsx'
+import Navigation from './routes/navigation/navigation-component.jsx'
+import Authentication from './routes/Authentication/authentication-component.jsx'
+import Shop from './routes/shop/shop-components.jsx'
+import CheckOut from './routes/checkout/checkout-component.jsx'
 
-import { checkUserSession } from "./store/user/user.action.js";
+import { checkUserSession } from './store/user/user.action.js'
 
 const App = () => {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    dispatch(checkUserSession());
-  }, []);
+    dispatch(checkUserSession())
+  }, [])
 
   return (
     // using Routes makes the browser expect a route inside the component (these components are just function names)
     // once the path string matches the route it renders the element
     <Routes>
-      <Route path="/" element={<Navigation />}>
+      <Route path='/' element={<Navigation />}>
         {/* index == true tells the route that when we match the "/" then the home component should be rendered too */}
         <Route index element={<Home />} />
-        <Route path="shop/*" element={<Shop />} />
-        <Route path="auth" element={<Authentication />} />
-        <Route path="checkout" element={<CheckOut />} />
+        <Route path='shop/*' element={<Shop />} />
+        <Route path='auth' element={<Authentication />} />
+        <Route path='checkout' element={<CheckOut />} />
       </Route>
     </Routes>
-  );
-};
+  )
+}
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,32 +1,35 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
+import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { Elements } from '@stripe/react-stripe-js'
 
-import App from "./App";
+import App from './App'
 
-import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from './reportWebVitals'
 
-import { store, persistor } from "./store/store";
-import "./index.scss";
+import { store, persistor } from './store/store'
+import './index.scss'
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 // by wrapping the App in the browser router there are some implementations that we are allowing the brower router to handle for us like keepng track of previous navigations
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
