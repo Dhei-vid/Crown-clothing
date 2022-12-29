2ed871f9f295624c09bbc2d1df67611a5fe3ebad
David <djetuk@gmail.com>
2022-12-29 17:05:01 +0100

    created styling for stripe form, and imported the stripe hooks

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,13 +1,30 @@
-import { CardElement } from '@stripe/react-stripe-js'
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js'
 
 import Button, { button_type_classes } from '../button/button-component'
+import { PaymentFormContainer, FormContainer } from './payment-form.styles'
 
 const PaymentForm = () => {
+  // used to make request in strip format
+  const stripe = useStripe()
+
+  const elements = useElements()
+
+  const paymentHandler = async e => {
+    e.preventDefault()
+
+    if (!stripe || !elements) return
+
+    // we need to make a fetch request to the backend to create a payment intent
+  }
+
   return (
-    <div>
-      <CardElement />
-      <Button button_type={button_type_classes.inverted}> Pay Now! </Button>
-    </div>
+    <PaymentFormContainer>
+      <FormContainer>
+        <h2> Credit Card Payment</h2>
+        <CardElement />
+        <Button button_type={button_type_classes.inverted}> Pay Now! </Button>
+      </FormContainer>
+    </PaymentFormContainer>
   )
 }
 
