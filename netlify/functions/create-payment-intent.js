a260edf6c8d74ac0712831350e2f50b3b5998f88
David <djetuk@gmail.com>
2023-01-01 21:08:31 +0100

    trying to fix the stripe not recognizing the API key

--- netlify/functions/create-payment-intent.js
+++ netlify/functions/create-payment-intent.js
@@ -1,5 +1,5 @@
-require('dotenv').config({ path: './.env' })
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+require('dotenv').config()
+const stripe = require('stripe')(`${process.env.STRIPE_SECRET_KEY}`)
 
 exports.handler = async event => {
   try {
