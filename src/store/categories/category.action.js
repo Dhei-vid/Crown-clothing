f8e09ac8a8870ccbbac0332b5b1cbb479ec5a6a7
David <djetuk@gmail.com>
2023-01-28 21:13:13 +0100

    migrating category action to typescript

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -2,9 +2,9 @@ import CATEGORY_ACTION_TYPES from "./category.types";
 
 import { createAction } from "../../utils/reducers/reducers.utils";
 
-// export const setCategories = (categories) => {
-//   return createAction(CATEGORY_ACTION_TYPES.SET_CURRENT_CATEGORY, categories);
-// };
+export const setCategories = (categories) => {
+  return createAction(CATEGORY_ACTION_TYPES.SET_CURRENT_CATEGORY, categories);
+};
 
 export const fetchCategoryStart = () => {
   return createAction(CATEGORY_ACTION_TYPES.FETCH_CURRENT_CATEGORY_START);
