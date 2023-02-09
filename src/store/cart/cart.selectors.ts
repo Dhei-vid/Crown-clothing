ff485c933f6e364033aabec3d28f1d44bde44e28
David <djetuk@gmail.com>
2023-02-09 15:44:50 +0100

    fix: documenting return values of selectors

--- src/store/cart/cart.selectors.ts
+++ src/store/cart/cart.selectors.ts
@@ -3,19 +3,24 @@ import { CartState } from "./cart.reducer";
 
 const selectCartReducer = (state: any): CartState => state.cart;
 
+// returns a boolean
 export const selectCartStatus = createSelector(
   [selectCartReducer],
   (cart) => cart.isCartOpen
 );
 
+// return an array
 export const selectCart = createSelector(
   [selectCartReducer],
   (cart) => cart.cartItems
 );
+
+// returns a number
 export const selectCartTotal = createSelector([selectCart], (cart) =>
   cart.reduce((total, items) => total + items.price * items.quantity, 0)
 );
 
+// returns a number
 export const selectCartCount = createSelector([selectCart], (cart) =>
   cart.reduce((count, items) => count + items.quantity, 0)
 );
