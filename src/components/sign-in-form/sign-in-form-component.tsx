94c04b9b541333c4d183e2ca2868f7d3fd148345
David <djetuk@gmail.com>
2023-02-21 08:33:51 +0100

    feat: migrated sign in form to typescript

---
+++ src/components/sign-in-form/sign-in-form-component.tsx
@@ -0,0 +1,92 @@
+import { useState, ChangeEvent, FormEvent } from "react";
+import { useDispatch } from "react-redux";
+import FormInput from "../form-input/form-input.component";
+import Button, { button_type_classes } from "../button/button-component";
+
+import {
+  googleSignInStart,
+  emailSignInStart,
+} from "../../store/user/user.action";
+
+import {
+  SignUpContainer,
+  Header,
+  ButtonContainer,
+} from "./sign-in-form-styles";
+
+// default fields
+const defaultFormField = {
+  email: "",
+  password: "",
+};
+
+// create the markup...
+const SignInForm = () => {
+  const dispatch = useDispatch();
+  const [formFields, setFormFields] = useState(defaultFormField);
+  const { email, password } = formFields;
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormField);
+  };
+
+  const signInWithGoogle = async () => dispatch(googleSignInStart());
+
+  // authenticate user
+  const onHandleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    try {
+      dispatch(emailSignInStart(email, password));
+      resetFormFields();
+    } catch (e: any) {
+      if (e.code === "auth/wrong-password") {
+        alert("Incorrect password");
+      } else if (e.code === "auth/user-not-found")
+        alert("Email does not exist");
+    }
+  };
+
+  const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  return (
+    <SignUpContainer>
+      <Header>Already have an account?</Header>
+      <h1>Sign In</h1>
+      <form onSubmit={onHandleSubmit}>
+        <FormInput
+          label="Email"
+          type="email"
+          required
+          onChange={onChangeHandler}
+          name="email"
+          value={email}
+        />
+        <FormInput
+          label="Password"
+          type="password"
+          required
+          onChange={onChangeHandler}
+          name="password"
+          value={password}
+        />
+        <ButtonContainer>
+          <Button type="submit">Sign In</Button>
+          <Button
+            type="button"
+            button_type={button_type_classes.google}
+            onClick={signInWithGoogle}
+          >
+            Google Sign In
+          </Button>
+        </ButtonContainer>
+      </form>
+    </SignUpContainer>
+  );
+};
+
+export default SignInForm;
