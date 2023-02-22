e60acc4ddc547e9ddfaef9ee598d97a5645db23b
David <djetuk@gmail.com>
2023-02-22 19:44:47 +0100

    feat: migrated cart item from react to typescript

---
+++ src/components/cart-item/cart-item-component.tsx
@@ -0,0 +1,25 @@
+import { FC } from "react";
+
+import {
+  CartItemsContainer,
+  Image,
+  ItemDetails,
+  Name,
+} from "./cart-item-styles";
+
+const CartItem = ({ cartitem }: any) => {
+  const { name, quantity, imageUrl, price } = cartitem;
+  return (
+    <CartItemsContainer>
+      <Image src={imageUrl} alt="product item" />
+      <ItemDetails>
+        <Name as="span"> {name} </Name>
+        <span>
+          {quantity} x ${price}
+        </span>
+      </ItemDetails>
+    </CartItemsContainer>
+  );
+};
+
+export default CartItem;
