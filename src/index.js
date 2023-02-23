8f42b527014731f348a51506388058135cf02bf1
David <djetuk@gmail.com>
2023-02-23 22:08:09 +0100

    fix: recent commit

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
-import { Elements } from '@stripe/react-stripe-js'
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { Elements } from "@stripe/react-stripe-js";
 
-import reportWebVitals from './reportWebVitals'
-import { stripePromise } from './utils/stripe/stripe.utils'
+import reportWebVitals from "./reportWebVitals";
+import { stripePromise } from "./utils/stripe/stripe.utils";
 
-import { store, persistor } from './store/store'
-import './index.scss'
+import { store, persistor } from "./store/store";
+import "./index.scss";
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 // by wrapping the App in the browser router there are some implementations that we are allowing the brower router to handle for us like keepng track of previous navigations
 root.render(
   <React.StrictMode>
@@ -27,9 +27,9 @@ root.render(
       </PersistGate>
     </Provider>
   </React.StrictMode>
-)
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+reportWebVitals();
