7d986872db7bf726d768b7fd22230bdc4b920063
David <djetuk@gmail.com>
2023-03-05 23:18:47 +0100

    feat: migrated code from react to typescript
    feat: set the type of cart item

---
+++ src/components/checkout-item/checkout-item.component.tsx
@@ -0,0 +1,57 @@
+import { FC } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { selectCart } from "../../store/cart/cart.selectors";
+
+import { CartItem } from "../../store/cart/cart.types";
+import {
+  addItemToCart,
+  removeItemFromCart,
+  clearCartItem,
+} from "../../store/cart/cart.action";
+import {
+  CheckoutItemContainer,
+  ImageContainer,
+  Image,
+  Name,
+  Quantity,
+  Price,
+  Arrow,
+  Value,
+  RemoveButton,
+} from "./checkout-item.styles.js";
+
+type CheckOutItem = {
+  cartItem: CartItem;
+};
+
+const CheckoutItem: FC<CheckOutItem> = ({ cartItem }) => {
+  const { name, imageUrl, quantity, price } = cartItem;
+  const cartItems = useSelector(selectCart);
+  const dispatch = useDispatch();
+
+  const removeItemHandler = () =>
+    dispatch(removeItemFromCart(cartItems, cartItem));
+  const addItemHandler = () => dispatch(addItemToCart(cartItems, cartItem));
+  const clearItemHandler = () => dispatch(clearCartItem(cartItems, cartItem));
+
+  return (
+    <CheckoutItemContainer>
+      <ImageContainer>
+        <Image src={imageUrl} alt="added product to cart" />
+      </ImageContainer>
+      <Name as="span">{name} </Name>
+      <Quantity as="span">
+        <Arrow onClick={removeItemHandler}>&lt;</Arrow>
+        <Value>{quantity}</Value>
+        <Arrow onClick={addItemHandler}>&gt;</Arrow>
+      </Quantity>
+      <Price as="span"> ${price * quantity} </Price>
+      <RemoveButton as="span" onClick={clearItemHandler}>
+        &#10005;
+      </RemoveButton>
+    </CheckoutItemContainer>
+  );
+};
+
+export default CheckoutItem;
