e6cbd729339f4ec56010ed363db95ce94778aca8
David <djetuk@gmail.com>
2023-03-05 20:43:04 +0100

    feat: migrated from react to typescript, added types for title and products

---
+++ src/components/category-preview/category-preview.component.tsx
@@ -0,0 +1,33 @@
+import { FC } from "react";
+import ProductCard from "../product-card/product-card.component";
+import { CategoryItem } from "../../store/categories/category.types";
+
+import {
+  CategoryPreviewContainer,
+  Title,
+  Preview,
+} from "./category-preview.styles.jsx";
+
+export type CategoryPreviewTypes = {
+  title: string;
+  products: CategoryItem[];
+};
+
+const CategoryPreview: FC<CategoryPreviewTypes> = ({ title, products }) => {
+  return (
+    <CategoryPreviewContainer>
+      <h2>
+        <Title to={title}>{title.toUpperCase()}</Title>
+      </h2>
+      <Preview>
+        {products
+          .filter((_, index) => index < 4)
+          .map((prod) => (
+            <ProductCard key={prod.id} products={prod} />
+          ))}
+      </Preview>
+    </CategoryPreviewContainer>
+  );
+};
+
+export default CategoryPreview;
