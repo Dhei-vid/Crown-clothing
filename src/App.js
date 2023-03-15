3f66b1e7aa645ea19041fdf2d58f6c144c091a04
David <djetuk@gmail.com>
2023-03-15 23:30:04 +0100

    feat: optimizing code using code-splitting for different components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import { useEffect } from "react";
+import { useEffect, Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
-import Home from "./routes/home/home-components";
-import Navigation from "./routes/navigation/navigation-component";
-import Authentication from "./routes/Authentication/authentication-component";
-import Shop from "./routes/shop/shop-components";
-import CheckOut from "./routes/checkout/checkout-component";
+import Spinner from "./components/spinner/spinner.component";
 
 import { checkUserSession } from "./store/user/user.action";
+
+const SplitBundler = (link) => lazy(() => import(`${link}`));
+
+const Home = SplitBundler("./routes/home/home-components");
+const Authentication = SplitBundler(
+  "./routes/Authentication/authentication-component"
+);
+const Shop = SplitBundler("./routes/shop/shop-components");
+const CheckOut = SplitBundler("./routes/checkout/checkout-component");
+const Navigation = SplitBundler("./routes/navigation/navigation-component");
 
 const App = () => {
   const dispatch = useDispatch();
@@ -18,17 +24,17 @@ const App = () => {
   }, []);
 
   return (
-    // using Routes makes the browser expect a route inside the component (these components are just function names)
-    // once the path string matches the route it renders the element
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        {/* index == true tells the route that when we match the "/" then the home component should be rendered too */}
-        <Route index element={<Home />} />
-        <Route path="shop/*" element={<Shop />} />
-        <Route path="auth" element={<Authentication />} />
-        <Route path="checkout" element={<CheckOut />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          {/* index == true tells the route that when we match the "/" then the home component should be rendered too */}
+          <Route index element={<Home />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="auth" element={<Authentication />} />
+          <Route path="checkout" element={<CheckOut />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
