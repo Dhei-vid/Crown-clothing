44aa5ed06bead81fc525a7421456629639231930
David <djetuk@gmail.com>
2023-04-01 23:18:46 +0100

    feat: adding necessary TypeScript  files for PWA

--- src/routes/Authentication/authentication-styles.jsx
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const AuthenticationContainer = styled.div`
-  margin: 0 4rem;
-  display: flex;
-  justify-content: space-between;
-  gap: 5em;
-`;
